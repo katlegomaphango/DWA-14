6c34c2ddbb2f282820b8fe37aa543c14c796a4cd
katlego Maphango <mphangokatlego08@gmail.com>
2023-06-21 19:38:12 +0200

    styling

--- components/tally-app.js
+++ components/tally-app.js
@@ -1,18 +1,38 @@
 //@ts-check
 
-import { LitElement, html } from "../libs/lit-fw.js";
+import { LitElement, html, css } from "../libs/lit-fw.js";
+
+/**
+ * @typedef {object} State
+ * @prop {boolean} Idle
+ * @prop {boolean} Max
+ * @prop {boolean} Min
+ */
+
+/**
+ * @type {State}
+ */
+// const STATE = {
+//     Idle: true,
+//     Max: true,
+//     Min: true
+// }
 
 class TallyApp extends LitElement {
 
     static properties = {
         value: { type: String },
-        open: {type: Boolean }
+        open: {type: Boolean },
+        state: {type: String},
+        min: {type: String},
+        max: {type: String},
     }
 
     constructor() {
         super()
         this.value = 0
         this.open = false
+        this.state = 'idle'
     }
 
     minusHandler() {
@@ -29,6 +49,79 @@ class TallyApp extends LitElement {
         setTimeout(() => this.open = !this.open, 3000)
     }
 
+    static styles = css`
+        * {
+            box-sizing: border-box;
+        }
+
+        /* header */
+        .header {
+            text-align: center;
+        }
+
+        .header__title {
+            font-size: 3rem;
+            font-weight: 900;
+            color: var(--color-light-gray);
+        }
+
+        /* .counter__value::part(base) */
+        .counter-value {
+            width: 100%;
+            height: 15rem;
+            font-size: 10rem;
+            font-weight: 900;
+            background: none;
+            border-width: 0;
+            border-bottom: 1px solid var(--color-light-gray);
+            color: var(--color-white);
+            text-align: center;
+            justify-items: center;
+        }
+
+        /* counter */
+        .counter-main {
+            background: var(--color-dark-gray);
+            max-width: 50%;
+            margin: 0 auto;
+        }
+
+        .counter-actions {
+            width: 100%;
+            display: grid;
+            grid-template-rows: 1fr 1fr;
+            row-gap: 1rem;
+        }
+
+        .counter-actions>div {
+            /* background-color: #fff; */
+            display: flex;
+            width: 100%;
+        }
+
+        /* .counter__buttons */
+        .btn {
+            width: 50%;
+            background: none;
+            border-width: 0;
+            font-size: 3rem;
+            border-bottom: 1px solid var(--color-light-gray);
+        }
+
+        .btn-minus {
+            border-right: 1px solid var(--color-light-gray);
+        }
+
+        .counter__buttons::part(label) {
+            color: black;
+        }
+
+        .btn:active, .btn-reset:active {
+            background: var(--color-medium-gray);
+            transform: translateY(-2%);
+        }
+    `
+
     render() {
         return html`
             <section>
@@ -42,9 +135,9 @@ class TallyApp extends LitElement {
                     <div class="counter-actions">
                         <div>
                             <button class="btn btn-minus" @click=${this.minusHandler}>-</button>
-                            <button class="btn btn-minus" @click=${this.addHandler}>+</button>
+                            <button class="btn btn-add" @click=${this.addHandler}>+</button>
                         </div>
-                        <button class="btn btn-reset" @click=${this.resetHandler}>Reset</button>
+                        <button class="btn-reset" @click=${this.resetHandler}>Reset</button>
                     </div>
                 </main>
                 <footer class="footer">
